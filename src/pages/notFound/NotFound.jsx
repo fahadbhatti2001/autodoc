d6c59ce383fbdf6b29dc85961ff5622e9546edd8
Fahad Bhatti <83200973+fahadbhatti2001@users.noreply.github.com>
2023-06-22 23:21:03 +0500

    Update NotFound.jsx

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -5,7 +5,7 @@ const notFound = () => {
   return (
     <section>
       <div className="container notfound__container">
-        <h2>Page Not Found</h2>
+        <h2>Page Not Found.</h2>
         <Link to="/" className="btn">
           Go Back Home
         </Link>
